a907a088adc60911ad8e8549e9dd830e42ad71de
hwang joon seon <48309309+hjs926@users.noreply.github.com>
2023-04-05 13:31:31 +0900

    Add files via upload

---
+++ precache-manifest.abe38da6c5f06b0e0ee91ef868711ce4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93e4013bd253efad90f9fd1bd669c44",
+    "url": "/JJSHOP/index.html"
+  },
+  {
+    "revision": "a47c33f11bc962f7ff5b",
+    "url": "/JJSHOP/static/css/2.a1251ad2.chunk.css"
+  },
+  {
+    "revision": "d46a076af845439141cf",
+    "url": "/JJSHOP/static/css/main.35a93780.chunk.css"
+  },
+  {
+    "revision": "a47c33f11bc962f7ff5b",
+    "url": "/JJSHOP/static/js/2.1d26f6f8.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/JJSHOP/static/js/2.1d26f6f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46a076af845439141cf",
+    "url": "/JJSHOP/static/js/main.65d460e1.chunk.js"
+  },
+  {
+    "revision": "77b33d395ac078ec8975",
+    "url": "/JJSHOP/static/js/runtime-main.10988327.js"
+  }
+]);
